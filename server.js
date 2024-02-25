e5fc98fcf596356fc182d54fcdc29b36ef37a122
Ayush750502 <ayush750502@gmail.com>
2024-02-25 23:50:51 +0530

    main file for running first page

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({extended : true}));
+app.set('view engine' , 'ejs');
+app.get("/" , logger, logger, logger , (req , res  /* , next*/) => {
+    console.log("App has Started!");
+    // res.send("Hi");// genrally used for testing our code.
+    // res.sendStatus(500);// for sending errors
+    // res.json({message: "Error"}); // for setting api
+    //res.download("README.md");// for sending a file
+    res.render("index" , {text0: "world"});// 
+    console.log("App has ended!");
+});
+
+
+const userRouter = require('./routes/users');// adding user library
+app.use('/users' , userRouter);// adding route to the server
+const publicRouter = require('./routes/public');// adding user library
+app.use('/public' , publicRouter);// adding route to the server
+
+function logger(req , res , next){
+    console.log(`At page: ${req.originalUrl}`);// gives url after port number
+    next();
+}
+
+app.listen(3000);
+console.log("Sever is running at 'http://localhost:3000'");
